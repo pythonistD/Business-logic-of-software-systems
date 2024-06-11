0dc9c267b5b0e8856a9ca3fb1128de108f01f157
Cheboksarov Yaroslav <yacheboksarov768@gmail.com>
2024-06-11 18:56:52 +0300

    final commit: profiles added

--- src/main/java/cheboksarov/blps_lab1/BlpsLab1Application.java
+++ src/main/java/cheboksarov/blps_lab1/BlpsLab1Application.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BlpsLab1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlpsLab1Application.class, args);
+		SpringApplication app = new SpringApplication(BlpsLab1Application.class);
+		//app.setAdditionalProfiles("helios");
+		app.run(args);
 	}
 
 }
